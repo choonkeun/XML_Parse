030ff4987ff8b716434aae1f1d8c09cb4a9e3ead
Choonkeun <choonkeun@yahoo.com>
2022-03-24 12:09:43 -0700

    unzip files
    unzip files

---
+++ XMLParse/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace XMLParse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.inputFileName = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnBackward = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnCopyText = new System.Windows.Forms.Button();
+            this.btnParseXml = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtDataText = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.Controls.Add(this.lblCount);
+            this.groupBox1.Controls.Add(this.inputFileName);
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.btnBackward);
+            this.groupBox1.Controls.Add(this.btnForward);
+            this.groupBox1.Controls.Add(this.txtSearch);
+            this.groupBox1.Controls.Add(this.btnCopyText);
+            this.groupBox1.Controls.Add(this.btnParseXml);
+            this.groupBox1.Controls.Add(this.btnOpenFile);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(881, 43);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblCount
+            // 
+            this.lblCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(503, 17);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(45, 13);
+            this.lblCount.TabIndex = 8;
+            this.lblCount.Text = "lblCount";
+            this.lblCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // inputFileName
+            // 
+            this.inputFileName.AutoSize = true;
+            this.inputFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputFileName.Location = new System.Drawing.Point(289, 16);
+            this.inputFileName.Name = "inputFileName";
+            this.inputFileName.Size = new System.Drawing.Size(78, 16);
+            this.inputFileName.TabIndex = 7;
+            this.inputFileName.Text = "lblFilename";
+            this.inputFileName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(233, 12);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(50, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnBackward
+            // 
+            this.btnBackward.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBackward.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBackward.Location = new System.Drawing.Point(804, 12);
+            this.btnBackward.Name = "btnBackward";
+            this.btnBackward.Size = new System.Drawing.Size(71, 23);
+            this.btnBackward.TabIndex = 5;
+            this.btnBackward.Text = "Backward";
+            this.btnBackward.UseVisualStyleBackColor = true;
+            this.btnBackward.Click += new System.EventHandler(this.btnBackward_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnForward.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForward.Location = new System.Drawing.Point(736, 12);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(65, 23);
+            this.btnForward.TabIndex = 4;
+            this.btnForward.Text = "Forward";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSearch.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(564, 12);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(166, 23);
+            this.txtSearch.TabIndex = 3;
+            this.txtSearch.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnCopyText
+            // 
+            this.btnCopyText.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopyText.Location = new System.Drawing.Point(187, 12);
+            this.btnCopyText.Name = "btnCopyText";
+            this.btnCopyText.Size = new System.Drawing.Size(45, 23);
+            this.btnCopyText.TabIndex = 2;
+            this.btnCopyText.Text = "Copy";
+            this.btnCopyText.UseVisualStyleBackColor = true;
+            this.btnCopyText.Click += new System.EventHandler(this.btnCopyText_Click);
+            // 
+            // btnParseXml
+            // 
+            this.btnParseXml.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnParseXml.Location = new System.Drawing.Point(90, 12);
+            this.btnParseXml.Name = "btnParseXml";
+            this.btnParseXml.Size = new System.Drawing.Size(96, 23);
+            this.btnParseXml.TabIndex = 1;
+            this.btnParseXml.Text = "2.Parse XML";
+            this.btnParseXml.UseVisualStyleBackColor = true;
+            this.btnParseXml.Click += new System.EventHandler(this.btnParseXml_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenFile.Location = new System.Drawing.Point(6, 12);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(81, 23);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "1.OpenFile";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox2.Controls.Add(this.txtDataText);
+            this.groupBox2.Location = new System.Drawing.Point(12, 61);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(881, 576);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // txtDataText
+            // 
+            this.txtDataText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDataText.AutoWordSelection = true;
+            this.txtDataText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDataText.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDataText.Location = new System.Drawing.Point(6, 16);
+            this.txtDataText.Name = "txtDataText";
+            this.txtDataText.Size = new System.Drawing.Size(869, 551);
+            this.txtDataText.TabIndex = 0;
+            this.txtDataText.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 649);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnParseXml;
+        private System.Windows.Forms.Button btnCopyText;
+        private System.Windows.Forms.Button btnBackward;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.RichTextBox txtDataText;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label inputFileName;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ XMLParse/Form1.cs
@@ -0,0 +1,295 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace XMLParse
+{
+    public partial class Form1 : Form
+    {
+        string version = "XMLParse (v1.0)";
+        int searchStrPos = 0;
+        string oldSearchStr = string.Empty;
+
+        OpenFileDialog openFileDialog1 = new OpenFileDialog();
+        string extension = string.Empty;
+        string filename = string.Empty;
+        string filenameNoExtension = string.Empty;
+        string fileroot = string.Empty;
+        string filepath = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = version;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.Filter = "All files (*.*)|*.*";
+            lblCount.Visible = false;
+            inputFileName.Visible = false;
+
+            //Anker
+            groupBox1.Anchor = AnchorStyles.Top |                AnchorStyles.Left | AnchorStyles.Right;
+            groupBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtDataText.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+
+            //text area
+            txtDataText.SelectAll();
+            txtDataText.SelectionFont = new Font("Calibri", 11);
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK)                      // Test result.
+            {
+                string selectFileName = openFileDialog1.FileName;
+                extension = System.IO.Path.GetExtension(selectFileName);
+                filename = System.IO.Path.GetFileName(selectFileName);
+                filenameNoExtension = System.IO.Path.GetFileNameWithoutExtension(selectFileName);
+                fileroot = System.IO.Path.GetPathRoot(selectFileName);
+                filepath = System.IO.Path.GetDirectoryName(selectFileName);
+
+                FileInfo info = new FileInfo(selectFileName);
+                long filesize = info.Length;
+                if (filesize > 2147483647)
+                {
+                    inputFileName.Text = "Too big file size(Max is 2,147,483,647 bytes)";
+                }
+                else
+                {
+                    this.Text = version + " - " + filepath;
+                    inputFileName.Text = filename;
+                    txtDataText.Text = File.ReadAllText(info.FullName);
+                }
+                inputFileName.Visible = true;
+            }
+            else
+            {
+                inputFileName.Text = string.Empty;
+            }
+
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtDataText.Text = string.Empty;
+            txtSearch.Text = string.Empty;
+            searchStrPos = 0;
+            lblCount.Visible = false;
+            inputFileName.Visible = false;
+        }
+
+        private void btnCopyText_Click(object sender, EventArgs e)
+        {
+            Clipboard.Clear();
+            Clipboard.SetText(txtDataText.Text);
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            txtSearch.Text = txtSearch.Text.Trim();
+            int founded = Regex.Matches(txtDataText.Text, txtSearch.Text).Count;
+
+            if (txtSearch.Text == string.Empty) return;
+            if (oldSearchStr != txtSearch.Text)
+            {
+                oldSearchStr = txtSearch.Text;
+                searchStrPos = 0;
+
+                txtDataText.SelectionStart = 0;
+                txtDataText.SelectionLength = txtDataText.TextLength;
+                txtDataText.SelectionColor = Color.Black;
+
+                lblCount.Visible = true;
+                lblCount.Text = "Wait.";
+            }
+
+            if (txtDataText.SelectionStart < searchStrPos) 
+                searchStrPos = txtDataText.SelectionStart;
+
+            if (searchStrPos < txtDataText.TextLength)
+            {
+                int count = 0;
+                searchStrPos++;
+                searchStrPos = Utility.HighlightText(txtDataText, txtSearch.Text, Color.Red, searchStrPos, ref count);
+                count = count > 0 ? count : 1;      //if not found, set 1
+                lblCount.Text = (founded - count + 1).ToString() + " of " + founded.ToString();
+                txtDataText.SelectionStart = searchStrPos;
+                txtDataText.Focus();
+            }
+            else
+            {
+                searchStrPos = 0;
+            }
+        }
+
+        private void btnBackward_Click(object sender, EventArgs e)
+        {
+            txtSearch.Text = txtSearch.Text.Trim();
+            int founded = Regex.Matches(txtDataText.Text, txtSearch.Text).Count;
+
+            if (txtSearch.Text == string.Empty) return;
+            if (oldSearchStr != txtSearch.Text)
+            {
+                oldSearchStr = txtSearch.Text;
+                searchStrPos = txtDataText.TextLength;
+
+                txtDataText.SelectionStart = 0;
+                txtDataText.SelectionLength = txtDataText.TextLength;
+                txtDataText.SelectionColor = Color.Black;
+
+                lblCount.Visible = true;
+                lblCount.Text = "Wait.";
+            }
+
+            //get current curso position and check
+            if (txtDataText.SelectionStart > searchStrPos)
+                searchStrPos = txtDataText.SelectionStart;
+
+            if (searchStrPos > 0)
+            {
+                int count = 0;
+                searchStrPos--;
+                searchStrPos = Utility.Back_HighlightText(txtDataText, txtSearch.Text, Color.Red, searchStrPos, ref count);
+                count = count > 0 ? count : 1;      //if not found, set 1
+                lblCount.Text = (founded - count + 1).ToString() + " of " + founded.ToString();
+                txtDataText.SelectionStart = searchStrPos;
+                txtDataText.Focus();
+            }
+            else
+            {
+                searchStrPos = 0;
+            }
+        }
+
+        private void btnParseXml_Click(object sender, EventArgs e)
+        {
+            if (txtDataText.TextLength < 1) return;
+
+            txtDataText.SelectAll();
+            txtDataText.SelectionFont = new Font("Consolas", 10);
+
+            string xmloutput = String.Empty;
+            xmloutput = txtDataText.Text;
+
+            System.Xml.XmlDocument oDoc = new System.Xml.XmlDocument();
+            try
+            {
+                //Boolean isXML = oDoc.LoadXml(XMLData);
+                oDoc.LoadXml(xmloutput);
+                txtDataText.Text = "";
+                display_node(oDoc.ChildNodes, -4);
+            }
+            catch (Exception ex)
+            {
+                txtDataText.Text = ex.Message;
+            }
+
+            txtDataText.SelectAll();
+            txtDataText.SelectionFont = new Font("Consolas", 10);
+
+            //https://msdn.microsoft.com/en-us/library/system.windows.forms.sendkeys.send(v=vs.110).aspx
+            //SendKeys.Send("{HOME}");
+            System.Media.SystemSounds.Beep.Play();
+        }
+
+        private void display_node(System.Xml.XmlNodeList Nodes, int Indent)
+        {
+            Indent += 4;    //Insert space left to make indent
+
+            foreach (System.Xml.XmlNode xNode in Nodes)
+            {
+                if (xNode.NodeType == XmlNodeType.Element || xNode.NodeType == XmlNodeType.Text)
+                {
+                    if (xNode.NodeType == XmlNodeType.Element)
+                    {
+                        txtDataText.Text += System.Environment.NewLine;
+                        txtDataText.Text += new String(' ', Indent);
+                        txtDataText.Text += xNode.Name + ": ";
+
+                        if (xNode.Attributes.Count > 0)
+                        {
+                            for (int i = 0; i < xNode.Attributes.Count; i++)
+                            {
+                                txtDataText.Text += xNode.Attributes[i].Name + ": " + xNode.Attributes[i].Value + " ";
+                                if (i != xNode.Attributes.Count - 1)
+                                    txtDataText.Text += ", ";
+                            }
+                        }
+                    }
+                    txtDataText.Text += string.IsNullOrEmpty(xNode.Value) ? "" : xNode.Value;
+                }
+                if (xNode.HasChildNodes)
+                {
+                    display_node(xNode.ChildNodes, Indent);
+                }
+            }
+
+        }
+
+
+    }
+
+
+
+    static class Utility {
+        public static int HighlightText(RichTextBox myRtb, string word, Color color, int startIndex, ref int count)
+        {
+            bool found = false;
+            int forwardPos = startIndex;
+            int index;
+            count = 0;
+
+            while ((index = myRtb.Text.IndexOf(word, startIndex)) != -1)
+            {
+                if (forwardPos < index)
+                {
+                    count++;                    //count all matches
+                    if (!found)
+                    {
+                        forwardPos = index;     //save first found position
+                        found = true;
+                    }
+                }
+
+                myRtb.Select(index, word.Length);
+                myRtb.SelectionColor = color;
+
+                startIndex = index + word.Length;
+            }
+            return forwardPos;
+        }
+        public static int Back_HighlightText(RichTextBox myRtb, string word, Color color, int startIndex, ref int count)
+        {
+            bool found = false;
+            int pos = startIndex;
+            int index;
+            count = 0;
+
+            while ((index = myRtb.Text.LastIndexOf(word, startIndex)) != -1)
+            {
+                count++;
+                if (pos > index && !found)
+                {
+                    pos = index;
+                    found = true;
+                }
+
+                myRtb.Select(index, word.Length);
+                myRtb.SelectionColor = color;
+
+                startIndex = index - word.Length;
+            }
+            return pos;
+        }
+    }
+
+}
